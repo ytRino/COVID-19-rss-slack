4eba3a0fa5f19be4766d85753e8fd44435ffc19b
Yutaro Iino <ytrino@gmail.com>
2020-01-29 11:44:06 +0900

    Only science/medical feed until check duplication between latest category

--- functions/src/Constant/rssItem.ts
+++ functions/src/Constant/rssItem.ts
@@ -1,5 +1,5 @@
 const rssItems = new Map<string, string>([
-    ["NHK", "https://www.nhk.or.jp/rss/news/cat0.xml"],
+//    ["NHK", "https://www.nhk.or.jp/rss/news/cat0.xml"],
     ["NHK科学医療", "https://www.nhk.or.jp/rss/news/cat3.xml"]
 ]);
 
