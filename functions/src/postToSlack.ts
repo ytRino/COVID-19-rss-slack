a211526811f9fbee368dca4bbe9d93dfead827bb
Yutaro Iino <ytrino@gmail.com>
2020-02-28 19:46:55 +0900

    Mas webhook URL (exposed URL is already invalidated by slack automatically!

--- functions/src/postToSlack.ts
+++ functions/src/postToSlack.ts
@@ -1,8 +1,8 @@
 const request = require('request');
- 
+
 const postToSlack = (url: string) => {
   request.post({
-    uri: "https://hooks.slack.com/services/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",
+    uri: "https://hooks.slack.com/services/xxxxxxxxxxxxxxxxxxxxxxx",
     headers: { 'Content-type': 'application/json' },
     json: { 'text': url }
   });
